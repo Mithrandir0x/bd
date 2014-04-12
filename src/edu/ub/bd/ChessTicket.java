bfc8498f70cb2a5c83bab5ca097c887a33965dbb
Mithrandir0x <mithrandir0x@gmail.com>
2014-04-12 11:53:25 +0200

    Added base project

---
+++ src/edu/ub/bd/ChessTicket.java
@@ -0,0 +1,16 @@
+package edu.ub.bd;
+
+/**
+ *
+ * @author olopezsa13
+ */
+public class ChessTicket
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+    }
+}
