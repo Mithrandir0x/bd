b2765c7755149ef27734dfe46079140626f716f4
Mithrandir0x <mithrandir0x@gmail.com>
2014-05-01 18:10:09 +0200

    Updated queries

--- db/db_inserts.py
+++ db/db_inserts.py
@@ -4,10 +4,11 @@ from datetime import date
 
 hotels_map = {}
 players_map = {}
+referees_map = {}
 games_chambers_map = {}
 
 def countries_sql():
-    insert_query_1 = 'INSERT INTO "ESTAT" ("ID", "NOM", "CODI_ISO") VALUES (\'%s\', \'%s\', \'%s\');'
+    insert_query_1 = 'INSERT INTO "PAIS" ("NOM") VALUES (\'%s\');'
 
     countries = ( "Alemanya",
         "Anglaterra",
@@ -22,42 +23,70 @@ def countries_sql():
         "Índia" )
 
     for i, name in enumerate(countries, 1):
-        print insert_query_1 % ( i, name, '' )
+        print insert_query_1 % ( name )
 
 def hotels_sql():
     global hotels_map
 
-    insert_query_1 = 'INSERT INTO "HOTELS" ("ID", "NOM", "CIUTAT_ID") VALUES (\'%s\', \'%s\', \'%s\');'
-    insert_query_2 = 'INSERT INTO "SALA" ("ID", "NOM", "AFORAMENT", "HOTEL_ID") VALUES (\'%s\', \'%s\', \'%s\', \'%s\');'
+    insert_query_1 = 'INSERT INTO "HOTEL" ("NOM") VALUES (\'%s\');'
+    insert_query_2 = 'INSERT INTO "SALA" ("HOTEL", "NOM", "AFORAMENT") VALUES (\'%s\', \'%s\', \'%s\');'
 
-    hotels = ( "Hotel Capri",
-        "Hotel Antemare",
-        "Hotel Montserrat",
-        "Hotel Calipolis" )
+    hotels = ( 'Hotel Antemare',
+        'Hotel Caesar Palace',
+        'Hotel Calipolis',
+        'Hotel Capri',
+        'Hotel Flamingo',
+        'Hotel Hilton',
+        'Hotel Holiday Inn',
+        'Hotel Melià',
+        'Hotel Miracle',
+        'Hotel Montserrat',
+        'Hotel Terramar' )
 
-    chambers = ( "Gènova;Hotel Capri;25;1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8",
-        "Venecia;Hotel Antemare;10;1.9,1.10,1.11,1.12,1.13,1.14,1.15,1.16",
-        "Victòria;Hotel Montserrat;20;2.1,2.2,2.3,2.4,2.5,2.6,2.7,2.8",
-        "Victòria;Hotel Calipolis;15;3.1,3.2,3.3,3.4,4.1,4.2,5.1" )
+    chambers = ( "Gènova;Hotel Capri;25;1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8;25",
+        "Venecia;Hotel Antemare;10;1.9,1.10,1.11,1.12,1.13,1.14,1.15,1.16;10",
+        "Victòria;Hotel Montserrat;20;2.1,2.2,2.3,2.4,2.5,2.6,2.7,2.8;18",
+        "Victòria;Hotel Calipolis;15;3.1,3.2,3.3,3.4;8",
+        "Ducci;Hotel Montserrat;12;4.1,4.2;",
+        "Artemisa;Hotel Montserrat;30;5.1;" )
 
     for i, name in enumerate(hotels, 1):
-        print insert_query_1 % ( i, name, '' )
-        hotels_map[name] = i
+        print insert_query_1 % ( name )
+        hotels_map[name] = name
 
     print
 
     for i, c in enumerate(chambers, 1):
         data = c.split(';')
         name = data[0]
-        hotel_id = hotels_map[data[1]]
+        hotel_fk = hotels_map[data[1]]
         capacity = data[2]
-        print insert_query_2 % ( i, name, capacity, hotel_id )
+        print insert_query_2 % ( hotel_fk, name, capacity )
         for gameday in data[3].split(','):
-            games_chambers_map[gameday] = i
+            games_chambers_map[gameday] = ( hotel_fk, name )
 
 def vendors_sql():
-    insert_query_1 = 'INSERT INTO "USUARI" ("ID", "NOM", "DNI", "SEXE", "TELEFON", "TIPUS_USUARI") VALUES (\'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\');'
-    insert_query_2 = 'INSERT INTO "USUARI_TAQUILLER" ("USUARI_ID", "ENTRADES_VENUDES") VALUES (\'%s\', \'%s\');'
+    insert_query_1 = ( ''
+            'INSERT INTO "TAQUILLER" ('
+                '"DNI", '
+                '"NOM", '
+                '"TELEFON", '
+                '"NOM_SISTEMA", '
+                '"CONTRASENYA", '
+                '"GENERE", '
+                '"PAIS", '
+                '"ENTRADES_VENUDES"'
+            ') VALUES ('
+                "'%s',"
+                "'%s',"
+                "'%s',"
+                "NULL,"
+                "NULL,"
+                "'%s',"
+                "'%s',"
+                "'%s'"
+            ');'
+        )
 
     vendors = ( "Maria,32013847R,Sra.,683372388",
         "Pere,38227338T,Sr.,633728377",
@@ -67,17 +96,37 @@ def vendors_sql():
         data = v.split(',')
         name = data[0]
         dni = data[1]
-        sex = data[2]
+        gender = data[2]
         ph = data[3]
-        if sex == 'Sr.': sex = 'M'
-        elif sex == 'Sra.': sex = 'F'
-        print insert_query_1 % ( i, name, dni, sex, ph, 'TAQUILLER' )
-        print insert_query_2 % ( i, 0 )
+        print insert_query_1 % ( dni, name, ph, gender, 'Espanya', 0 )
 
 def referees_sql():
-    insert_query_1 = 'INSERT INTO "USUARI" ("ID", "NOM", "COGNOM", "DNI", "SEXE", "TIPUS_USUARI") VALUES (\'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\');'
-    insert_query_2 = 'INSERT INTO "USUARI_JUTGE" ("USUARI_ID", "VICTORIES_BLANQUES", "VICTORIES_NEGRES", "VICTORIES_TAULES") VALUES (\'%s\', \'%s\', \'%s\', \'%s\');'
+    global referees_map
 
+    insert_query_1 = ( ''
+            'INSERT INTO "JUTGE" ('
+                '"DNI", '
+                '"NOM", '
+                '"TELEFON", '
+                '"GENERE", '
+                '"HOTEL", '
+                '"PAIS", '
+                '"TAULES", '
+                '"VICTORIES_BLANQUES", '
+                '"VICTORIES_NEGRES"'
+            ') VALUES ('
+                "'%s',"
+                "'%s',"
+                "NULL,"
+                "'%s',"
+                "'%s',"
+                "'%s',"
+                "'%s',"
+                "'%s',"
+                "'%s'"
+            ');'
+        )
+    
     referees = ( "Benjumea Azlatiletamendi,20100393R,Sr.,França,Hotel Capri",
         "Illiech Vladimir,20200288L,Sra.,Estats Units,Hotel Capri",
         "Pérez Lucián,20300643G,Sr.,Rússia,Hotel Capri",
@@ -85,24 +134,39 @@ def referees_sql():
 
     for i, v in enumerate(referees, 500):
         data = v.split(',')
-        name = ''
-        surname = ''
-        name = data[0].split(" ")[0]
-        if ( len(data[0].split(" ")) > 1 ):
-            surname = data[0].split(" ")[1]
+        name = data[0]
         dni = data[1]
-        sex = data[2]
+        gender = data[2]
         country = data[3]
-        if sex == 'Sr.': sex = 'M'
-        elif sex == 'Sra.': sex = 'F'
-        print insert_query_1 % ( i, name, surname, dni, sex, 'JUTGE' )
-        print insert_query_2 % ( i, 0, 0, 0 )
+        hotel = data[4]
+        print insert_query_1 % ( dni, name, gender, hotel, country, 0, 0, 0 )
+
+        referees_map[name] = dni
 
 def players_sql():
     global players_map
     
-    insert_query_1 = 'INSERT INTO "USUARI" ("ID", "NOM", "COGNOMS", "DNI", "SEXE", "DATA_NAIXEMENT", "TIPUS_USUARI") VALUES (\'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\');'
-    insert_query_2 = 'INSERT INTO "USUARI_JUGADOR" ("USUARI_ID", "PARTIDES_GUANYADES", "PARTIDES_PERDUDES") VALUES (\'%s\', \'%s\', \'%s\');'
+    insert_query_1 = ( ''
+            'INSERT INTO "JUGADOR" ('
+                '"DNI", '
+                '"NOM", '
+                '"TELEFON", '
+                '"GENERE", '
+                '"HOTEL", '
+                '"PAIS", '
+                '"PARTIDES_PERDUDES", '
+                '"PARTIDES_GUANYADES"'
+            ') VALUES ('
+                "'%s',"
+                "'%s',"
+                "NULL,"
+                "'%s',"
+                "%s,"
+                "'%s',"
+                "'%s',"
+                "'%s'"
+            ');'
+        )
 
     players = ( "Alexander Morozevich;11800;Sr.;Rússia;48;Hotel Capri;",
         "Anatoli Karpov;10300;Sr.;Rússia;38;Hotel Antemare;",
@@ -139,73 +203,107 @@ def players_sql():
     today = date.today()
     for i, p in enumerate(players, 1000):
         data = p.split(';')
-        
-        name = ''
-        surname = ''
-        name = data[0].split(" ")[0]
-        if ( len(data[0].split(" ")) > 1 ):
-            surname = data[0].split(" ")[1]
-        
+        name = data[0]
         dni = data[1]
-        
-        sex = data[2]
-        if sex == 'Sr.': sex = 'M'
-        elif sex == 'Sra.': sex = 'F'
-        
+        gender = data[2]
         country = data[3]
-        
         age = data[4]
+        hotel = data[5]
+        if ( len(hotel) > 0 ):
+            hotel = "'%s'" % hotel
+        else:
+            hotel = 'NULL'
+        
         year_born = today.year - int(age)
         
-        print insert_query_1 % ( i, name, surname, dni, sex, "%s-%s-%s" % ( year_born, today.month, today.day ), 'JUGADOR' )
-        print insert_query_2 % ( i, 0, 0 )
+        print insert_query_1 % ( dni, name, gender, hotel, country, 0, 0 )
 
-        players_map[data[0]] = i
+        players_map[name] = dni
+
+def gamedays_sql():
+    insert_query_1 = 'INSERT INTO "JORNADA" ("ID", "DATA_REALITZACIO") VALUES (\'%s\', NULL);'
+    gamedays = [i for i in range(1, 6)]
+    for i, g in enumerate(gamedays):
+        print insert_query_1 % ( g )
 
 def games_sql():
     global players_map
+    
+    insert_query_1 = ( ''
+            'INSERT INTO "PARTIDA" ('
+                '"ID", '
+                '"GUANYADOR", '
+                '"JUTGE", '
+                '"BLANQUES", '
+                '"NEGRES", '
+                '"JORNADA", '
+                '"HOTEL", '
+                '"SALA", '
+                '"ENTRADES_VENUDES"'
+            ') VALUES ('
+                "'%s',"
+                "NULL,"
+                "'%s',"
+                "'%s',"
+                "'%s',"
+                "'%s',"
+                "'%s',"
+                "'%s',"
+                "'%s'"
+            ');'
+        )
+    insert_query_2 = 'INSERT INTO "MOVIMENT" ("PARTIDA", "ORDRE", "DESCRIPCIO") VALUES (\'%s\', \'%s\', \'%s\');'
 
-    insert_query_1 = 'INSERT INTO "PARTIDA" ("ID", "JUGADOR_BLANQUES", "JUGADOR_NEGRES", "GUANYADOR", "VENCUT", "DURACIO", "JORNADA_ID", "SALA_ID") VALUES (\'%s\', \'%s\', \'%s\', NULL, NULL, \'%s\', \'%s\', \'%s\');'
-
-    games = ( "1.1,Bobby Fischer,Garry Kasparov",
-        "1.2,Mijail Tahl,Anatoli Karpov",
-        "1.3,Mijail Botvínnik,Viswanathan Anand",
-        "1.4,Paul Keres,Topalov",
-        "1.5,Wilhelm Steinitz,Maya Chiburdanidze",
-        "1.6,Charles Stanley,Paul Morphy",
-        "1.7,George Henry Mackenzie,S. Lipschütz",
-        "1.8,Magnus Carlsen,Boris Gelfand",
-        "1.9,Fabiano Caruana,Evgeny Alekseev",
-        "1.10,Alexander Morozevich,Étienne Bacrot",
-        "1.11,Levon Aronian,Wang Hao",
-        "1.12,Shakhriyar Mamedyarov,Viktor Laznicka",
-        "1.13,Ray Robson,Larry Christiansen",
-        "1.14,Robert Hess,Anish Girl",
-        "1.15,Emanuel Lasker,Max Euwe",
-        "1.16,José Raúl Capablanca,Judit Polgar",
-        "2.1,Anatoli Karpov,Bobby Fischer",
-        "2.2,Viswanathan Anand,Topalov",
-        "2.3,Charles Stanley,Wilhelm Steinitz",
-        "2.4,S. Lipschütz,Boris Gelfand",
-        "2.5,Fabiano Caruana,Étienne Bacrot",
-        "2.6,Levon Aronian,Shakhriyar Mamedyarov",
-        "2.7,Larry Christiansen,Anish Girl",
-        "2.8,Emanuel Lasker,Judit Polgar",
-        "3.1,Bobby Fischer,Viswanathan Anand",
-        "3.2,Charles Stanley,Boris Gelfand",
-        "3.3,Fabiano Caruana,Levon Aronian",
-        "3.4,Anish Girl,Judit Polgar",
-        "4.1,Boris Gelfand,Bobby Fischer",
-        "4.2,Judit Polgar,Fabiano Caruana" )
+    games = ( "1.1;Bobby Fischer;Garry Kasparov;Benjumea Azlatiletamendi",
+        "1.2;Mijail Tahl;Anatoli Karpov;Benjumea Azlatiletamendi ",
+        "1.3;Mijail Botvínnik;Viswanathan Anand;Benjumea Azlatiletamendi",
+        "1.4;Paul Keres;Topalov;Illiech Vladimir",
+        "1.5;Wilhelm Steinitz;Maya Chiburdanidze;Benjumea Azlatiletamendi",
+        "1.6;Charles Stanley; Paul Morphy;Benjumea Azlatiletamendi",
+        "1.7;George Henry Mackenzie;S. Lipschütz;Benjumea Azlatiletamendi",
+        "1.8;Magnus Carlsen;Boris Gelfand;Benjumea Azlatiletamendi",
+        "1.9;Fabiano Caruana;Evgeny Alekseev;Benjumea Azlatiletamendi",
+        "1.10;Alexander Morozevich;Étienne Bacrot;Illiech Vladimir",
+        "1.11;Levon Aronian;Wang Hao;Illiech Vladimir",
+        "1.12;Shakhriyar Mamedyarov;Viktor Laznicka;Illiech Vladimir",
+        "1.13;Ray Robson;Larry Christiansen;Pérez Lucián",
+        "1.14;Robert Hess;Anish Girl;Pérez Lucián",
+        "1.15;Emanuel Lasker;Max Euwe;Pérez Lucián",
+        "1.16;José Raúl Capablanca;Judit Polgar;Pérez Lucián",
+        "2.1;Anatoli Karpov;Bobby Fischer;Roberto Mandini",
+        "2.2;Viswanathan Anand; Topalov;Roberto Mandini",
+        "2.3;Charles Stanley;Wilhelm Steinitz;Roberto Mandini",
+        "2.4;S. Lipschütz;Boris Gelfand;Roberto Mandini",
+        "2.5;Fabiano Caruana;Étienne Bacrot;Benjumea Azlatiletamendi",
+        "2.6;Levon Aronian;Shakhriyar Mamedyarov;Illiech Vladimir",
+        "2.7;Larry Christiansen;Anish Girl;Pérez Lucián",
+        "2.8;Emanuel Lasker;Judit Polgar;Pérez Lucián",
+        "3.1;Bobby Fischer;Viswanathan Anand;Pérez Lucián",
+        "3.2;Charles Stanley;Boris Gelfand;Pérez Lucián",
+        "3.3;Fabiano Caruana;Levon Aronian;Pérez Lucián",
+        "3.4;Anish Girl;Judit Polgar;Roberto Mandini",
+        "4.1;Boris Gelfand;Bobby Fischer;Roberto Mandini",
+        "4.2;Judit Polgar;Fabiano Caruana;Pérez Lucián" )
 
     for i, g in enumerate(games, 1):
-        data = g.split(',')
-        gameday_id = data[0].split('.')[0]
-        white_id = players_map[data[1]]
-        black_id = players_map[data[2]]
-        chamber_id = games_chambers_map[data[0]]
-        print insert_query_1 % ( i, white_id, black_id, 0, gameday_id, chamber_id )
-
+        data = g.split(';')
+        gameday = data[0]
+        gameday_fk = data[0].split('.')[0]
+        white_fk = players_map[data[1].strip()]
+        black_fk = players_map[data[2].strip()]
+        referee_fk = referees_map[data[3].strip()]
+        hotel_fk = games_chambers_map[gameday][0]
+        chamber_fk = games_chambers_map[gameday][1]
+        print insert_query_1 % ( i, referee_fk, white_fk, black_fk, gameday_fk, hotel_fk, chamber_fk, 0 )
+        # try:
+        #     with open('./moves/' + gameday + '.txt') as file:
+        #         moves = file.readlines()
+        #         for j, m in enumerate(moves):
+        #             m = m.strip()
+        #             if len(m) > 0: print insert_query_2 % ( i, j, m )
+        # except IOError:
+        #     # print 'File not found "%s.txt"' % gameday
+        #     pass
 
 if __name__ == '__main__':
     countries_sql()
@@ -217,6 +315,8 @@ if __name__ == '__main__':
     referees_sql()
     print
     players_sql()
+    print
+    gamedays_sql()
     print
     games_sql()
     print
--- db/tables.sql
+++ db/tables.sql
@@ -1,155 +1,39 @@
 
--- Client based commands to execute when importing data:
---
--- \SET ON_ERROR_STOP ON
--- \SET client_min_messages TO NOTICE
---
-
--- ----------------------------------------------------------------------------------------------------------
--- START:[DROPS]
-
--- Tables
-DROP TABLE IF EXISTS "ENTRADA";
-DROP TABLE IF EXISTS "PARTIDES_JUGADORS";
 DROP TABLE IF EXISTS "MOVIMENT";
 DROP TABLE IF EXISTS "PARTIDA";
+DROP TABLE IF EXISTS "JUTGE";
+DROP TABLE IF EXISTS "JUGADOR";
+DROP TABLE IF EXISTS "TAQUILLER";
 DROP TABLE IF EXISTS "JORNADA";
-DROP TABLE IF EXISTS "TORNEIG";
 DROP TABLE IF EXISTS "SALA";
 DROP TABLE IF EXISTS "HOTEL";
-DROP TABLE IF EXISTS "USUARI_TAQUILLER";
-DROP TABLE IF EXISTS "USUARI_JUTGE";
-DROP TABLE IF EXISTS "USUARI_JUGADOR";
-DROP TABLE IF EXISTS "USUARI";
-DROP TABLE IF EXISTS "CIUTAT";
-DROP TABLE IF EXISTS "ESTAT";
--- Functions
--- Triggers
-
--- END:[DROPS]
--- ----------------------------------------------------------------------------------------------------------
-
-
--- ----------------------------------------------------------------------------------------------------------
--- START:[COMMON]
+DROP TABLE IF EXISTS "PAIS";
 
 -- -----------------------------------------------------
--- "ESTAT"
+-- "PAIS"
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS "ESTAT" (
-  "ID" INTEGER NOT NULL,
-  "NOM" VARCHAR(45) NOT NULL,
-  "CODI_ISO" VARCHAR(2) NOT NULL,
-  PRIMARY KEY ("ID")
-);
-
--- -----------------------------------------------------
--- "CIUTAT"
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS "CIUTAT" (
-  "ID" INTEGER NOT NULL,
-  "NOM" VARCHAR(45) NULL,
-  "ESTAT_ID" INTEGER NOT NULL,
-  PRIMARY KEY ("ID"),
-  FOREIGN KEY ("ESTAT_ID") REFERENCES "ESTAT" ("ID")
-);
-
--- END:[COMMON]
--- ----------------------------------------------------------------------------------------------------------
-
-
--- ----------------------------------------------------------------------------------------------------------
--- START:[USER SPACE]
-
--- -----------------------------------------------------
--- "USUARI"
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS "USUARI" (
-  "ID" INTEGER NOT NULL,
+CREATE TABLE IF NOT EXISTS "PAIS" (
   "NOM" VARCHAR(32) NOT NULL,
-  "COGNOMS" VARCHAR(32) NOT NULL,
-  "NOM_SISTEMA" VARCHAR(32) NOT NULL,
-  "CONTRASENYA" VARCHAR(32) NOT NULL,
-  "TIPUS_USUARI" VARCHAR(32) NOT NULL,
-  "ESTAT_ID" INTEGER NOT NULL,
-  PRIMARY KEY ("ID"),
-  FOREIGN KEY ("ESTAT_ID") REFERENCES "ESTAT" ("ID")
+  PRIMARY KEY ("NOM")
 );
-
--- -----------------------------------------------------
--- "JUGADOR"
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS "USUARI_JUGADOR" (
-  "USUARI_ID" INTEGER NOT NULL,
-  "PARTIDES_GUANYADES" INTEGER DEFAULT 0,
-  "PARTIDES_PERDUDES" INTEGER DEFAULT 0,
-  FOREIGN KEY ("USUARI_ID") REFERENCES "USUARI" ("ID")
-);
-
--- -----------------------------------------------------
--- "JUTGE"
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS "USUARI_JUTGE" (
-  "USUARI_ID" INTEGER NOT NULL,
-  "VICTORIES_BLANQUES" INTEGER DEFAULT 0,
-  "VICTORIES_NEGRES" INTEGER DEFAULT 0,
-  "VICTORIES_TAULES" INTEGER DEFAULT 0,
-  FOREIGN KEY ("USUARI_ID") REFERENCES "USUARI" ("ID")
-);
-
--- -----------------------------------------------------
--- "TAQUILLER"
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS "USUARI_TAQUILLER" (
-  "USUARI_ID" INTEGER NOT NULL,
-  "ENTRADES_VENUDES" INTEGER,
-  FOREIGN KEY ("USUARI_ID") REFERENCES "USUARI" ("ID")
-);
-
--- END:[USER SPACE]
--- ----------------------------------------------------------------------------------------------------------
-
-
--- ----------------------------------------------------------------------------------------------------------
--- START:[HOTELS]
 
 -- -----------------------------------------------------
 -- "HOTEL"
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS "HOTEL" (
-  "ID" INTEGER NOT NULL,
   "NOM" VARCHAR(45) NULL,
-  "CIUTAT_ID" INTEGER NOT NULL,
-  PRIMARY KEY ("ID"),
-  FOREIGN KEY ("CIUTAT_ID") REFERENCES "CIUTAT" ("ID")
+  PRIMARY KEY ("NOM")
 );
 
 -- -----------------------------------------------------
 -- "SALA"
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS "SALA" (
-  "ID" INTEGER NOT NULL,
+  "HOTEL" VARCHAR(45) NOT NULL,
   "NOM" VARCHAR(45) NULL,
   "AFORAMENT" INTEGER DEFAULT 0,
-  "HOTEL_ID" INTEGER NOT NULL,
-  PRIMARY KEY ("ID"),
-  FOREIGN KEY ("HOTEL_ID") REFERENCES "HOTEL" ("ID")
-);
-
--- END:[HOTELS]
--- ----------------------------------------------------------------------------------------------------------
-
-
--- ----------------------------------------------------------------------------------------------------------
--- START:[GAMES]
-
--- -----------------------------------------------------
--- "TORNEIG"
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS "TORNEIG" (
-  "ID" INTEGER NOT NULL,
-  "TITOL" VARCHAR(45) NULL,
-  PRIMARY KEY ("ID")
+  PRIMARY KEY ("HOTEL", "NOM"),
+  FOREIGN KEY ("HOTEL") REFERENCES "HOTEL" ("NOM")
 );
 
 -- -----------------------------------------------------
@@ -157,11 +41,66 @@ CREATE TABLE IF NOT EXISTS "TORNEIG" (
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS "JORNADA" (
   "ID" INTEGER NOT NULL,
-  "DATA_REALITZACIO" TIMESTAMP NULL,
   "ORDRE" INTEGER NULL,
-  "TORNEIG_ID" INTEGER NOT NULL,
-  PRIMARY KEY ("ID"),
-  FOREIGN KEY ("TORNEIG_ID") REFERENCES "TORNEIG" ("ID")
+  "DATA_REALITZACIO" TIMESTAMP NULL,
+  PRIMARY KEY ("ID")
+);
+
+--   "GENERE" DOMAIN {'Sr.', 'Sra.'}
+
+-- -----------------------------------------------------
+-- "TAQUILLER"
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS "TAQUILLER" (
+  "DNI" VARCHAR(16) NOT NULL,
+  "NOM" VARCHAR(32) NOT NULL,
+  "TELEFON" VARCHAR(32) NULL,
+  "NOM_SISTEMA" VARCHAR(32) NULL,
+  "CONTRASENYA" VARCHAR(32) NULL,
+  "GENERE" VARCHAR(4) NOT NULL,
+  "ENTRADES_VENUDES" INTEGER DEFAULT 0,
+  "PAIS" VARCHAR(32) NOT NULL,
+  PRIMARY KEY ("DNI"),
+  FOREIGN KEY ("PAIS") REFERENCES "PAIS" ("NOM")
+);
+
+-- -----------------------------------------------------
+-- "JUGADOR"
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS "JUGADOR" (
+  "DNI" VARCHAR(16) NOT NULL,
+  "NOM" VARCHAR(32) NOT NULL,
+  "TELEFON" VARCHAR(32) NULL,
+  "NOM_SISTEMA" VARCHAR(32) NULL,
+  "CONTRASENYA" VARCHAR(32) NULL,
+  "GENERE" VARCHAR(4) NOT NULL,
+  "PARTIDES_GUANYADES" INTEGER DEFAULT 0,
+  "PARTIDES_PERDUDES" INTEGER DEFAULT 0,
+  "HOTEL" VARCHAR(45) NULL,
+  "PAIS" VARCHAR(32) NOT NULL,
+  PRIMARY KEY ("DNI"),
+  FOREIGN KEY ("HOTEL") REFERENCES "HOTEL" ("NOM"),
+  FOREIGN KEY ("PAIS") REFERENCES "PAIS" ("NOM")
+);
+
+-- -----------------------------------------------------
+-- "JUTGE"
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS "JUTGE" (
+  "DNI" VARCHAR(16) NOT NULL,
+  "NOM" VARCHAR(32) NOT NULL,
+  "TELEFON" VARCHAR(32) NULL,
+  "NOM_SISTEMA" VARCHAR(32) NULL,
+  "CONTRASENYA" VARCHAR(32) NULL,
+  "GENERE" VARCHAR(4) NOT NULL,
+  "VICTORIES_BLANQUES" INTEGER DEFAULT 0,
+  "VICTORIES_NEGRES" INTEGER DEFAULT 0,
+  "TAULES" INTEGER DEFAULT 0,
+  "HOTEL" VARCHAR(45) NULL,
+  "PAIS" VARCHAR(32) NOT NULL,
+  PRIMARY KEY ("DNI"),
+  FOREIGN KEY ("HOTEL") REFERENCES "HOTEL" ("NOM"),
+  FOREIGN KEY ("PAIS") REFERENCES "PAIS" ("NOM")
 );
 
 -- -----------------------------------------------------
@@ -170,64 +109,27 @@ CREATE TABLE IF NOT EXISTS "JORNADA" (
 CREATE TABLE IF NOT EXISTS "PARTIDA" (
   "ID" INTEGER NOT NULL,
   "GUANYADOR" INTEGER NULL,
-  "VENCUT" INTEGER NULL,
-  "DURACIO" INTEGER NULL,
-  "JORNADA_ID" INTEGER NOT NULL,
-  "SALA_ID" INTEGER NOT NULL,
+  "JUTGE" VARCHAR(16) NOT NULL,
+  "BLANQUES" VARCHAR(16) NOT NULL,
+  "NEGRES" VARCHAR(16) NOT NULL,
+  "JORNADA" INTEGER NOT NULL,
+  "HOTEL" VARCHAR(45) NOT NULL,
+  "SALA" VARCHAR(45) NOT NULL,
+  "ENTRADES_VENUDES" INTEGER DEFAULT 0,
   PRIMARY KEY ("ID"),
-  FOREIGN KEY ("JORNADA_ID") REFERENCES "JORNADA" ("ID"),
-  FOREIGN KEY ("SALA_ID") REFERENCES "SALA" ("ID")
+  FOREIGN KEY ("JUTGE") REFERENCES "JUTGE" ("DNI"),
+  FOREIGN KEY ("BLANQUES") REFERENCES "JUGADOR" ("DNI"),
+  FOREIGN KEY ("NEGRES") REFERENCES "JUGADOR" ("DNI"),
+  FOREIGN KEY ("JORNADA") REFERENCES "JORNADA" ("ID"),
+  FOREIGN KEY ("HOTEL", "SALA") REFERENCES "SALA" ("HOTEL", "NOM")
 );
 
 -- -----------------------------------------------------
 -- "MOVIMENT"
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS "MOVIMENT" (
-  "PARTIDA_ID" INTEGER NOT NULL,
-  "JUGADOR_ID" INTEGER NOT NULL,
+  "PARTIDA" INTEGER NOT NULL,
   "ORDRE" INTEGER NOT NULL,
-  "DESCRIPCIO" VARCHAR(48) NOT NULL,
-  "DURACIO" INTEGER NOT NULL,
-  FOREIGN KEY ("PARTIDA_ID") REFERENCES "PARTIDA" ("ID"),
-  FOREIGN KEY ("JUGADOR_ID") REFERENCES "JUGADOR" ("ID")
+  "DESCRIPCIO" VARCHAR(64) NOT NULL,
+  FOREIGN KEY ("PARTIDA") REFERENCES "PARTIDA" ("ID")
 );
-
--- -----------------------------------------------------
--- "PARTIDES_JUGADORS"
---
--- Domini "COLOR": {BLANC, NEGRE}
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS "PARTIDES_JUGADORS" (
-  "PARTIDA_ID" INTEGER NOT NULL,
-  "JUGADOR_ID" INTEGER NOT NULL,
-  "COLOR" VARCHAR(5) NOT NULL,
-  FOREIGN KEY ("PARTIDA_ID") REFERENCES "PARTIDA" ("ID"),
-  FOREIGN KEY ("JUGADOR_ID") REFERENCES "JUGADOR" ("ID")
-);
-
--- END:[GAMES]
--- ----------------------------------------------------------------------------------------------------------
-
--- ----------------------------------------------------------------------------------------------------------
--- START:[TRADING]
-
--- -----------------------------------------------------
--- "ENTRADA"
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS "ENTRADA" (
-  "ID" INTEGER NOT NULL,
-  "PREU_VENTA" INTEGER NOT NULL,
-  "TIPUS_PAGAMENT" INTEGER NOT NULL,
-  "TAQUILLER_ID" INTEGER NOT NULL,
-  "JORNADA_ID" INTEGER NOT NULL,
-  "SALA_ID" INTEGER NOT NULL,
-  FOREIGN KEY ("TAQUILLER_ID") REFERENCES "TAQUILLER" ("ID"),
-  FOREIGN KEY ("JORNADA_ID") REFERENCES "JORNADA" ("ID"),
-  FOREIGN KEY ("SALA_ID") REFERENCES "SALA" ("ID")
-);
-
--- END:[TRADING]
--- ----------------------------------------------------------------------------------------------------------
-
--- Selecciona les sales utilitzades en les partides d'una jornada
-SELECT s."ID", s."NOM", s."AFORAMENT" FROM "JORNADA" j, "PARTIDA" p, "SALA" s WHERE p."JORNADA_ID" = j."ID" AND j."ID" = '?';
