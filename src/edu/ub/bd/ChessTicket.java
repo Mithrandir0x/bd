a0808b4fc76d8120f070938e5bb6f263d10e26a2
Mithrandir0x <mithrandir0x@gmail.com>
2014-05-14 19:23:00 +0200

    Added Postgre jdbc driver

--- src/edu/ub/bd/ChessTicket.java
+++ src/edu/ub/bd/ChessTicket.java
@@ -1,16 +1,10 @@
 package edu.ub.bd;
 
-/**
- *
- * @author olopezsa13
- */
 public class ChessTicket
 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args)
     {
     }
+
 }
